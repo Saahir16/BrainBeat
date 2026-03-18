31f26cfc3542570794a86db73c9a2b4fc21ccc8b
Saahir16 <swkhan@asu.edu>
2026-03-17 17:39:30 -0700

    Create study_session_tracking.py with placeholder things to do
    Laid out what has to be coded up later for keeping track of the student's study sessions

---
+++ study_session_tracking.py
@@ -0,0 +1,15 @@
+# for how a student can time themselves
+
+def timeStart():
+  # start the time for a study session
+  pass
+def timeEnd():
+  # end the time for a study session
+  pass
+def sessionSummary():
+  # how long was whatever studied in a day
+  pass
+def sessionsStats():
+  # end of semester stats that has a bunch of reports
+  # need to connect to firebase for this
+  pass
